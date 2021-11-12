6cccd86d57ebc76d5e6ffadee5a91f3750a94669
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-12 19:59:34 +0600

    added quote Details

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { useParams, Outlet } from 'react-router-dom';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 1, author: 'Joney', text: 'Learning React is Awesome' },
+  {
+    id: 2,
+    author: 'jerry',
+    text: 'JavaScript is Awesome to Learn',
+  },
+];
 
 const QuoteDetail = () => {
   const { id } = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === parseInt(id));
+
   return (
-    <div>
-      <h1>QuoteDetail page</h1>
-      <h2>{id}</h2>
+    <>
+      <HighlightedQuote quote={quote} />
       <Outlet />
-    </div>
+    </>
   );
 };
 
