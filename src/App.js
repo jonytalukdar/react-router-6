6c2867fe84e4e5fd0daa4131d6ada69050584db8
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-12 15:03:38 +0600

    start for project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/welcome" />} />
-          <Route path="/welcome/*" element={<Welcome />}>
-            <Route path="new-user" element={<h3>new User</h3>} />
-          </Route>
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:productId" element={<ProductDetail />} />
-        </Routes>
-      </main>
+      <h1>hello</h1>
     </div>
   );
 }
 
 export default App;
-
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
-// our-domain.com/product-detail/a-book
