b6231cf717dd7acc834b678b76e3f8ff6ba7f3af
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-12 22:52:13 +0600

    added

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import QuoteForm from '../components/quotes/QuoteForm';
+import { useNavigate } from 'react-router-dom';
 
 const NewQuote = () => {
+  const navigate = useNavigate();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    navigate('/quotes', { replace: true });
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
