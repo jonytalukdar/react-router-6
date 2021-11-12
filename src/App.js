e2252aa70c76029be8f1f7e0e5f512df0a35fdd9
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-12 19:21:05 +0600

    added nested route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AllQuotes from './pages/AllQuotes';
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
 
+import Comments from './components/comments/Comments';
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AllQuotes />} />
-      <Route path="/quotes/:id" element={<QuoteDetail />} />
+      <Route path="/" element={<Navigate replace to="/quotes" />} />
+      <Route path="/quotes" element={<AllQuotes />} />
+      <Route path="/quotes/:id" element={<QuoteDetail />}>
+        <Route path="comments" element={<Comments />} />
+      </Route>
       <Route path="/new-quote" element={<NewQuote />} />
     </Routes>
   );
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams, Outlet } from 'react-router-dom';
 
 const QuoteDetail = () => {
+  const { id } = useParams();
   return (
     <div>
       <h1>QuoteDetail page</h1>
+      <h2>{id}</h2>
+      <Outlet />
     </div>
   );
 };
