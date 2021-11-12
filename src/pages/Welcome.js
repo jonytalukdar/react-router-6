7f0d3b5047dd248cf3e17c438827b69474d3d03d
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-12 11:30:54 +0600

    added

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from 'react-router';
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h2>Welcome page</h2>
+      {/* <Route path="/welcome/new-user">
+        <h3>new User</h3>
+      </Route> */}
+    </section>
+  );
 };
 
 export default Welcome;
