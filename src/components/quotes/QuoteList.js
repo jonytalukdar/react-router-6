d254e1ccc7a35294fa976cc48a7a259ec6a19d95
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-12 23:32:53 +0600

    added sorting

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,44 @@
-import { Fragment } from 'react';
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+// sorting function
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortAscending);
+
+  const changeSortHandler = () => {
+    navigate('/quotes?sort=' + (isSortAscending ? 'desc' : 'asc'), {
+      replace: true,
+    });
+  };
+
   return (
-    <Fragment>
+    <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>
+          Sort {isSortAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
@@ -16,7 +47,7 @@ const QuoteList = (props) => {
           />
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 };
 
